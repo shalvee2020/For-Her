508683e1fcef6ba0079a19bf4fa1f897e81c82e0
Shalvee <shalveekumari02@gmail.com>
2024-10-05 12:43:27 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAaBTYqnQDXWs2pO5VwLc3SP2bbO_DfmpY",
-    authDomain: "for-her-c8653.firebaseapp.com",
-    projectId: "for-her-c8653",
-    storageBucket: "for-her-c8653.appspot.com",
-    messagingSenderId: "362836800380",
-    appId: "1:362836800380:web:e18383e8c95b6e68c4aaf9",
-    measurementId: "G-4ZR0MVDY1S"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getAuth(app);
+export const database = getAuth(app);
