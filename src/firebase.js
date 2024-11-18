83fdaf53f9dea9753f73e43ffe46598096261d98
Shalvee Kumari <98416599+shalvee2020@users.noreply.github.com>
2024-11-18 19:38:00 +0530

    Update firebase.js as we are setting database as well

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_database_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -11,7 +14,7 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getAuth(app);
+export const auth = getAuth(app); // Export the auth instance
+export const database = getFirestore(app); // Initialize Firestore and export it
